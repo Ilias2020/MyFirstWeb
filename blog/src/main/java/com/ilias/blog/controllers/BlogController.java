55ae697ce01b9c42c5d6e02b19334f73feb0998d
ILIDZH <ilias.dzhumabekov@gmail.com>
2022-06-14 18:28:09 +0200

    Added the ability to edit articles.

--- blog/src/main/java/com/ilias/blog/controllers/BlogController.java
+++ blog/src/main/java/com/ilias/blog/controllers/BlogController.java
@@ -57,6 +57,21 @@ public class BlogController {
         model.addAttribute("post", res);
         return "blog-edit";
     }
+    @PostMapping("/blog/{id}/edit")
+    public  String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons,
+                               @RequestParam String full_text, Model model){
+        Post post = postRepo.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepo.save(post);
+        return "redirect:/blog";
+    }
 
-
+    @PostMapping("/blog/{id}/remove")
+    public  String blogPostDelete(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepo.findById(id).orElseThrow();
+        postRepo.delete(post);
+        return "redirect:/blog";
+    }
 }
